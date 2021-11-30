07a8a6932fee624920f7cdd0458729b83afbf5a9
miguel <m.gutierrez@peak-14.de>
2021-11-30 14:14:40 +0100

    Created services: - Create JokeService interface and implements method:   -> getRandomQuote closes #2

---
+++ src/main/java/com/example/chucknorrisquotestask/services/JokeService.java
@@ -0,0 +1,9 @@
+package com.example.chucknorrisquotestask.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface JokeService {
+    public String getRandomQuote();
+}
