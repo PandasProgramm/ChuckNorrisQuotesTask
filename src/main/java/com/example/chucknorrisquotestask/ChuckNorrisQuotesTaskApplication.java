ad731094023b463ae79193a20ee4937bbbf6647f
miguel <m.gutierrez@peak-14.de>
2021-11-30 13:17:18 +0100

    Initial commit

---
+++ src/main/java/com/example/chucknorrisquotestask/ChuckNorrisQuotesTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.chucknorrisquotestask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisQuotesTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisQuotesTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chucknorrisquotestask/ChuckNorrisQuotesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chucknorrisquotestask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisQuotesTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
